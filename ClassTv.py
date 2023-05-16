33fffdcf4aae778a96e9347e9dd335546027b8f9
KaninWithRice <mishalnorona014@gmail.com>
2023-05-17 02:11:08 +0800

    Str to Int

--- ClassTv.py
+++ ClassTv.py
@@ -1,14 +1,15 @@
 class TV:
-    def __init__(tv, name, channel, volumelevel):
+    def __init__(tv, name, channel, volumelevel, status):
         tv.name = name
         tv.cha = channel
         tv.vlvl = volumelevel
+        tv.stat = status
 
     def TestTV(tv):
-        print(tv.name + "channel is" + tv.cha + "and volume level is" + tv.vlvl)
-
-tv1 = TV("tv1", " 22 ", " 50 ")
-tv2 = TV("tv2", " 14 ", " 70 ")
+        print("Status: ", tv.stat)
+        print(tv.name, " channel is ", tv.cha, " and volume level is ", tv.vlvl)
+tv1 = TV("tv1's", 22, 50, "on")
+tv2 = TV("tv2's", 14, 70, "on")
 
 tv1.TestTV()
 tv2.TestTV()

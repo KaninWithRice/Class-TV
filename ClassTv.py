04d9670595e26e3c8c4207f72f956d294fa782ff
KaninWithRice <mishalnorona014@gmail.com>
2023-05-17 13:44:31 +0800

    Pseudocode Intro Animation

--- ClassTv.py
+++ ClassTv.py
@@ -1,8 +1,47 @@
+# Mohammad Mishal S. Noroña | BSCPE 1-5 | Assignment #6
+# Introduction
+print("")
+print("WELCOME TO THE PROGRAM".center(40," ") )
+print("By: Mishal Noroña".center(40," ") )
+# Add Loading Time
+import time
+import sys
+
+done = 'false'
+
+def animate():
+        sys.stdout.write('\rLoading Please Wait ▒▒▒▒▒▒▒▒▒▒  0%')
+        time.sleep(0.5)
+        sys.stdout.write('\rLoading Please Wait █▒▒▒▒▒▒▒▒▒  10%')
+        time.sleep(0.5)
+        sys.stdout.write('\rLoading Please Wait ██▒▒▒▒▒▒▒▒  20%')
+        time.sleep(0.5)
+        sys.stdout.write('\rLoading Please Wait ███▒▒▒▒▒▒▒  30%')
+        time.sleep(0.5)
+        sys.stdout.write('\rLoading Please Wait ████▒▒▒▒▒▒  40%')
+        time.sleep(0.5)
+        sys.stdout.write('\rLoading Please Wait █████▒▒▒▒▒  50%')
+        time.sleep(0.5)
+        sys.stdout.write('\rLoading Please Wait ██████▒▒▒▒  60%')
+        time.sleep(0.5)
+        sys.stdout.write('\rLoading Please Wait ███████▒▒▒  70%')
+        time.sleep(0.5)
+        sys.stdout.write('\rLoading Please Wait ████████▒▒  80%')
+        time.sleep(0.5)
+        sys.stdout.write('\rLoading Please Wait █████████▒  90%')
+        time.sleep(0.5)
+        sys.stdout.write('\rLoading Please Wait ██████████ 100%')
+        time.sleep(0.3)
+        sys.stdout.write('\nTHE TV IS ON     \n')
+
+animate()
+# Ask for user's input
 vol_input1 = int(input("TV1 Enter a Volume Level: "))
 cha_input1 = int(input("TV1 Enter a Channel Number: "))
 
 vol_input2 = int(input("TV2 Enter a Volume Level: "))
 cha_input2 = int(input("TV2 Enter a Channel Number: "))
+# Create a class
 class TV:
     def __init__(tv, name, channel, volumelevel, status):
         tv.name = name
@@ -13,25 +52,27 @@ class TV:
     def TestTV(tv):
         print("Status: ", tv.stat)
         print(tv.name, " channel is ", tv.cha, " and volume level is ", tv.vlvl)
+# Create the objects
 tv1 = TV("tv1's", cha_input1, vol_input1, "on")
 tv2 = TV("tv2's", cha_input2, vol_input2, "on")
-
+# Print output
 tv1.TestTV()
 tv2.TestTV()
-
+# Make a formula for volume up and channel up
 def chaplus(channel):
     return channel + 1
 def volplus(volumelevel):
     return volumelevel + 1
-    
+# Make a formula for volume down and channel down    
 def chamin(channel):
     return channel - 1
 def volmin(volumelevel):
     return volumelevel - 1
-
+# Create a remote 
 while True:
     switch = input("\n List of commands \n\n Off [off] \n Increase Volume [v+] \n Decrease Volume [v-] \n Increase Channel [c+] \n Decrease Channel [c-] \n Enter a command: ")
-    if switch == "v+":
+# If user enters v+ volume will go up
+    if switch == "v+": 
         class TV:
             def __init__(tv, name, channel, volumelevel, status):
                 tv.name = name
@@ -52,9 +93,8 @@ while True:
                 print("Status: ", tv.stat)
                 print(tv.name, " channel is ", tv.cha, " and volume level is ", tv.vlvl)
             tv1.TestTV()
-            tv2.TestTV()
-        
-
+            tv2.TestTV()       
+# If user enters v- volume will go down
     elif switch == "v-":
         class TV:
             def __init__(tv, name, channel, volumelevel, status):
@@ -77,8 +117,7 @@ while True:
                 print(tv.name, " channel is ", tv.cha, " and volume level is ", tv.vlvl)
             tv1.TestTV()
             tv2.TestTV()
-        
-
+# If user enters c+ channel will go up
     elif switch == "c+":
         class TV:
             def __init__(tv, name, channel, volumelevel, status):
@@ -101,7 +140,7 @@ while True:
                 print(tv.name, " channel is ", tv.cha, " and volume level is ", tv.vlvl)
             tv1.TestTV()
             tv2.TestTV()
-
+# If user enters c+ channel will go down
     elif switch == "c-":
         class TV:
             def __init__(tv, name, channel, volumelevel, status):
@@ -124,7 +163,7 @@ while True:
                 print(tv.name, " channel is ", tv.cha, " and volume level is ", tv.vlvl)
             tv1.TestTV()
             tv2.TestTV()
-        
+# If user enters off the TV will turn off        
     elif switch == "off":
         print("Thank you for using THE TELEVISION")
         break

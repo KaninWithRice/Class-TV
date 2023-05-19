f0f1141062eb6258f395554d9cbedcaec1e98b43
KaninWithRice <mishalnorona014@gmail.com>
2023-05-19 20:17:07 +0800

    First Commit

--- ClassTv.py
+++ ClassTv.py
@@ -1,17 +1,25 @@
-# Create a Class
 class TV:
-    def __init__(tv, name, channel, volumelevel, status):
+    def __init__(tv,name ,channel, volumelevel, status):
         tv.name = name
         tv.cha = channel
         tv.vlvl = volumelevel
         tv.stat = status
+        
+    def statustv(tv):
+        tv.status = True
+        print("Status: ON")
+    def statustv(tv):
+        tv.status = False
+        print("Status: OFF")
 
-    def TestTV(tv):
-        print("Status: ", tv.stat)
-        print(tv.name, " channel is ", tv.cha, " and volume level is ", tv.vlvl)
-# Create the objects
-tv1 = TV("tv1's", 2, 30, "on")
-tv2 = TV("tv2's", 12, 40, "on")
-# Print output
-tv1.TestTV()
-tv2.TestTV()
+    def get_channel_num(tv):
+        return tv.cha
+    def set_channel_num (tv, channel):
+        if 0 <= channel <= 120:
+            print (tv.name,"'s channel is", tv.cha)
+        else:
+            print ("Enter a channel Between (1-120)")
+            return tv.cha
+
+    def get_volume_lvl(tv):
+        return
